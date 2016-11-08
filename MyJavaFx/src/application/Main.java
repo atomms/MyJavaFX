40da71a85bd3a5631c76f4fa46c971a0bda0f2f8
Ernesto Ramiro <atomms@gmail.com>
2016-11-08 18:23:08 +0100

    functional

--- MyJavaFx/src/application/Main.java
+++ MyJavaFx/src/application/Main.java
@@ -1,21 +1,67 @@
 package application;
 	
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
+	
+
+	private AnchorPane rootLayout;
+	public Stage primaryStage;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,300,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
+			
+			this.primaryStage = primaryStage;
+			this.primaryStage.setTitle("AID School");
+			this.primaryStage.setX(200);
+			this.primaryStage.setY(200);
+			
+//			BorderPane root = new BorderPane();
+//			Scene scene = new Scene(root,300,400);
+//			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+//			primaryStage.setScene(scene);
+			
 			primaryStage.show();
+			
+			initRootLayout();
+			
 		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void initRootLayout() {
+		try {
+			// Load root layout from fxml file.
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Main.class.getResource("view/Layouts.fxml"));
+			rootLayout = (AnchorPane) loader.load();
+
+			// Show the scene containing the root layout.
+			Scene scene = new Scene(rootLayout, 600, 400);
+			scene.getStylesheets().add(
+					getClass().getResource("application.css").toExternalForm());
+			scene.getStylesheets().add("http://fonts.googleapis.com/css?family=Shadows+Into+Light");
+
+			primaryStage.setScene(scene);
+
+			// // Give the controller access to the main app.
+			// OneController controller = loader.getController();
+			// controller.setMainApp(this);
+
+			// imageRaw.setEffect(new GaussianBlur());
+			primaryStage.show();
+
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
---
+++ MyJavaFx/src/application/OneController.java
@@ -0,0 +1,72 @@
+package application;
+
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+/**
+ * The only controller for the whole application.
+ * 
+ * @author ernesto
+ */
+public class OneController {
+	
+	//not using yet
+	@FXML
+	private ImageView imageRaw;
+
+    // Reference to the main application
+    private Main mainApp;
+
+    /**
+     * Is called by the main application to give a reference back to itself.
+     * 
+     * @param mainApp
+     */
+    public void setMainApp(Main mainApp) {
+        this.mainApp = mainApp;
+    }
+
+	/**
+     * Opens an stage to send the profile, to be done...
+     */
+    
+    public void showSendProfile() {
+        try {
+            // Load the fxml file and create a new stage for the popup.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("view/Superheader.fxml"));
+            AnchorPane page = (AnchorPane) loader.load();
+            Stage sendStage = new Stage();
+            sendStage.setTitle("Superheader");
+            Scene scene = new Scene(page);
+            sendStage.setScene(scene);
+            
+            sendStage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    /**
+     * Method to handle the ImageView action in the Main stage 
+     */
+    
+    @FXML
+    private void handleSend() {
+    	showSendProfile();
+    }
+
+    /**
+     * Closes the application when clicked on Exit in the secondary stage
+     */
+    @FXML
+    private void handleClose() {
+   	        System.exit(0);
+    }
+    }
